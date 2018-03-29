85bdbd4dc5ef69d3790902fc165e804792fc8656
DaubaKao <ab6060118@gmail.com>
2018-03-29 16:29:35 +0800

    first commit

---
+++ src/components/scrollbar/scrollbar.tsx
@@ -0,0 +1,261 @@
+import * as React from 'react'
+import './scrollbar.scss'
+
+interface ScrollbarProps {
+  width?:number|string
+  height?:number|string
+}
+
+interface ScrollbarState {
+  trackAtTop:boolean
+  trackAtBottom:boolean
+}
+
+enum BUTTON_TYPE {
+  UP = 0,
+  DOWND
+}
+
+export default class Scrollbar extends React.Component<ScrollbarProps, ScrollbarState> {
+  refs:{[key:string]:HTMLElement}
+  moveFirstClickYPoint:number
+  originalScrollTop:number
+
+  constructor(props:ScrollbarProps) {
+    super(props)
+    this.state = {
+      trackAtTop: true,
+      trackAtBottom: false
+    }
+    this.handleWindowResize = this.handleWindowResize.bind(this)
+    this.handleMoveTrack = this.handleMoveTrack.bind(this)
+    this.handleTrackMouseUp = this.handleTrackMouseUp.bind(this)
+  }
+
+  componentWillMount() {
+    window.addEventListener('resize', this.handleWindowResize)
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.handleWindowResize)
+  }
+
+  componentDidMount() {
+    this.updateScrollbar()
+  }
+
+  componentDidUpdate(p:any, n:any) {
+    this.updateScrollbar()
+  }
+
+  updateScrollbar() {
+    let { content, track, bar } = this.refs
+
+    if(content.scrollHeight !== content.offsetHeight) {
+      bar.style.display = 'flex'
+      track.style.height = content.offsetHeight / content.scrollHeight * 100 + '%'
+    }
+    else {
+      bar.style.display = 'none'
+    }
+  }
+
+  handleWheelAndKeyup(e:React.WheelEvent<HTMLElement>|React.KeyboardEvent<HTMLElement>) {
+    let { content, track } = this.refs
+    let { trackAtTop, trackAtBottom } = this.state
+
+
+    if(e.type === 'keydown') {
+      let allowKey = [
+        33, /* pageUp */
+        34, /* pageDown */
+        35, /* home */
+        36, /* end */
+        38, /* up */
+        40, /* down */
+      ]
+
+      let { keyCode } = (e as React.KeyboardEvent<HTMLElement>)
+
+      e.stopPropagation();
+
+      if(allowKey.indexOf(keyCode) < 0) return
+
+      if(keyCode === 33) content.scrollTop -= content.offsetHeight
+      if(keyCode === 34) content.scrollTop += content.offsetHeight
+      if(keyCode === 35) content.scrollTop = content.scrollHeight
+      if(keyCode === 36) content.scrollTop = 0
+      if(keyCode === 38) content.scrollTop -= 30
+      if(keyCode === 40) content.scrollTop += 30
+    }
+    else {
+      let { deltaY } = (e as React.WheelEvent<HTMLElement>)
+      if(deltaY > 0) {
+        content.scrollTop += 30
+      }
+      else {
+        content.scrollTop -= 30
+      }
+    }
+
+    if(content.scrollTop !== 0 && content.scrollTop + content.offsetHeight !== content.scrollHeight)  {
+      e.stopPropagation();
+    }
+  }
+
+  handleScroll() {
+    let { content, track } = this.refs
+    let { trackAtTop, trackAtBottom } = this.state
+
+    track.style.top = content.scrollTop / content.scrollHeight * 100 + '%'
+
+    if(content.scrollTop === 0 && !trackAtTop) {
+      this.setState({trackAtTop: true})
+    }
+    if(content.scrollTop !== 0 && trackAtTop) {
+      this.setState({trackAtTop: false})
+    }
+    if(content.scrollTop + content.offsetHeight === content.scrollHeight && !trackAtBottom) {
+      this.setState({trackAtBottom: true})
+    }
+    if(content.scrollTop + content.offsetHeight !== content.scrollHeight && trackAtBottom) {
+      this.setState({trackAtBottom: false})
+    }
+  }
+
+  handleWindowResize() {
+    this.updateScrollbar()
+  }
+
+  handleBarBodyMouseDown(e:React.MouseEvent<HTMLElement>) {
+    let { content, bar, track, bar_body } = this.refs
+    let clickPoint:number
+    let clickPointOfBar:number
+    let interval:number
+    let barTop:number
+
+    barTop = bar_body.getBoundingClientRect().top
+    clickPoint = e.pageY
+    clickPointOfBar = clickPoint - bar_body.getBoundingClientRect().top
+
+
+    if(e.pageY < track.getBoundingClientRect().top) {
+      interval = window.setInterval(() => {
+        content.scrollTop -= 15
+        if(track.offsetTop <= clickPointOfBar - track.offsetHeight / 2) {
+          window.clearInterval(interval)
+        }
+      }, 10)
+    }
+    else {
+      interval = window.setInterval(() => {
+        content.scrollTop += 15
+        if(track.offsetTop >= clickPointOfBar - track.offsetHeight / 2) {
+          window.clearInterval(interval)
+        }
+      }, 10)
+    }
+
+    window.addEventListener('mouseup', () => {
+      window.clearInterval(interval)
+    }, { once: true })
+  }
+
+  handleTrackMouseDown(e:React.MouseEvent<HTMLElement>) {
+    let { content } = this.refs
+
+    e.stopPropagation()
+
+    this.moveFirstClickYPoint = e.clientY
+    this.originalScrollTop = content.scrollTop
+
+    window.addEventListener('mousemove', this.handleMoveTrack)
+    window.addEventListener('mouseup', this.handleTrackMouseUp)
+  }
+
+  handleTrackMouseUp(e:MouseEvent) {
+    window.removeEventListener('mousemove', this.handleMoveTrack)
+    window.removeEventListener('mouseup', this.handleTrackMouseUp)
+  }
+
+  handleMoveTrack(e:MouseEvent) {
+    let { content, bar_body } = this.refs
+    let delta = e.clientY - this.moveFirstClickYPoint 
+
+    content.scrollTop = this.originalScrollTop + delta / bar_body.offsetHeight * content.scrollHeight
+  }
+
+  handleUpDownClick(type:BUTTON_TYPE) {
+    return function() {
+      let { trackAtTop, trackAtBottom } = this.state
+      let { content } = this.refs
+      let interval:number
+
+      if(type === BUTTON_TYPE.UP && !trackAtTop) {
+        interval = window.setInterval(() => {
+          content.scrollTop -= 15
+        }, 10)
+      }
+      if(type === BUTTON_TYPE.DOWND && !trackAtBottom) {
+        interval = window.setInterval(() => {
+          content.scrollTop += 15
+        }, 10)
+      }
+
+      window.addEventListener('mouseup', () => {
+        window.clearInterval(interval)
+      }, { once: true })
+    }
+  }
+
+  getUpDownButtonClassName(type:BUTTON_TYPE) {
+    let className:string[] = []
+    let { content } = this.refs
+    let { trackAtTop, trackAtBottom } = this.state
+
+    if(!content) return
+
+    if(type === BUTTON_TYPE.UP) {
+      className.push('scrollbar-bar-up')
+      if(trackAtTop) className.push('disable')
+    }
+    else {
+      className.push('scrollbar-bar-down')
+      if(trackAtBottom) className.push('disable')
+    }
+
+    return className.join(' ')
+  }
+
+  render() {
+    let { children, width, height } = this.props
+    let scrollbarStyle = {
+      width:width,
+      maxWidth: width,
+      height:height,
+      maxHeight: height 
+    }
+
+    return(
+      <div className="scrollbar" style={scrollbarStyle} onWheel={this.handleWheelAndKeyup.bind(this)} onKeyDown={this.handleWheelAndKeyup.bind(this)} tabIndex={0}>
+        <div className="scrollbar-content" ref='content' onScroll={this.handleScroll.bind(this)}>
+          { children }
+        </div>
+        <div className="scrollbar-bar" ref='bar'>
+          <span
+            className={this.getUpDownButtonClassName(BUTTON_TYPE.UP)}
+            onMouseDown={this.handleUpDownClick(BUTTON_TYPE.UP).bind(this)}></span>
+          <div className="scrollbar-bar-body" ref="bar_body" onMouseDown={this.handleBarBodyMouseDown.bind(this)}>
+            <div 
+              className="scrollbar-bar-body-track"
+              ref="track"
+              onMouseDown={this.handleTrackMouseDown.bind(this)} ></div>
+          </div>
+          <span
+            className={this.getUpDownButtonClassName(BUTTON_TYPE.DOWND)}
+            onMouseDown={this.handleUpDownClick(BUTTON_TYPE.DOWND).bind(this)}></span>
+        </div>
+      </div>
+    )
+  }
+}
---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+const plugins = [
+  new HtmlWebpackPlugin( {
+    title: 'Hello',
+    template: __dirname + '/src/index.html'
+  }),
+  new ExtractTextPlugin({
+    filename: "bundle.css",
+    disable: process.env.NODE_ENV !== "prod"
+  })
+]
+
+module.exports = {
+  entry: __dirname + "/src/index.tsx",
+  output: {
+    filename: "bundle.js",
+    path: __dirname + "/dist"
+  },
+
+  // Enable sourcemaps for debugging webpack's output.
+  devtool: "source-map",
+
+  resolve: {
+    // Add '.ts' and '.tsx' as resolvable extensions.
+    extensions: [".ts", ".tsx", ".js", ".json"]
+  },
+
+  module: {
+    rules: [
+      // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+      { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+
+      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
+
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader']
+        })
+      },
+
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192
+            }
+          }
+        ]
+      }
+    ]
+  },
+
+  plugins: plugins,
+
+  devServer: {
+    host: '0.0.0.0',
+    port: '9000',
+    hot: true,
+  }
+};
