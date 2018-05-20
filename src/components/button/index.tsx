bc5990d63415d582f8eeaa4ac200c91784dd791a
DaubaKao <ab6060118@gmail.com>
2018-05-21 01:41:03 +0800

    [button] update layout

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -2,60 +2,13 @@ import * as React from 'react'
 import './style.scss'
 
 interface IButton {
-  handleClick:Function
   disabled:boolean
-  text:string
+  handleClick:React.MouseEventHandler<HTMLElement>
 }
 
-interface IButtonState {
-  isClicked:boolean
-}
-
-export default class Button extends React.Component<IButton, IButtonState> {
-  constructor(props:IButton) {
-    super(props)
-
-    this.state = {
-      isClicked: false
-    }
-
-    this.handleMouseUp = this.handleMouseUp.bind(this)
-  }
-
-  handleClick() {
-    if(this.props.disabled === true) {
-      return
-    }
-
-    this.props.handleClick()
-  }
-
-  handleMouseUp() {
-    if(this.props.disabled === true) {
-      return
-    }
-
-    this.setState({isClicked: false})
-
-    document.removeEventListener('mouseup', this.handleMouseUp)
-  }
-
-  handleMouseDown() {
-    if(this.props.disabled === true) {
-      return
-    }
-
-    document.addEventListener('mouseup', this.handleMouseUp)
-
-    this.setState({isClicked: true})
-  }
-
+export default class Button extends React.Component<IButton> {
   getClass() {
     let className:string[] = ['default-button']
-
-    if(this.state.isClicked) {
-      className.push('pressed')
-    }
 
     if(this.props.disabled === true) {
       className.push('disabled')
@@ -65,14 +18,14 @@ export default class Button extends React.Component<IButton, IButtonState> {
   }
 
   render() {
-    let { text } = this.props
+    let { children, disabled, handleClick } = this.props
 
     return (
       <button
         className={this.getClass()}
-        onClick={this.handleClick.bind(this)}
-        onMouseDown={this.handleMouseDown.bind(this)}>
-      {text}
+        onClick={handleClick}
+        disabled={disabled}>
+      {children}
       </button>
     )
   }
